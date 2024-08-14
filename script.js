22bca732ce9c7cc328bfb03fe5d1a9993e59daac
Ashish <90548985+aashxs@users.noreply.github.com>
2024-08-14 15:43:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let btn=document.querySelector("#btn");
+let inc=document.querySelector("#inc");
+let i=1;
+btn.addEventListener("click",()=>{
+    
+    inc.innerHTML=i;
+   alert(i);
+    i++;
+    
+
+})
